d52ed89fbc18a04167fd473e86e15788c4c52575
LuisZMarques <2212522@my.ipleiria.pt>
2022-08-11 12:27:12 +0100

    Working but need to check CSS

---
+++ app.js
@@ -0,0 +1,7 @@
+const sideBar = document.querySelector(".sidebar");
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+
+toggleBtn.addEventListener("click", function(){
+    sideBar.classList.toggle("show-sidebar");
+})
